ced38e825b272c5147dda8a61ec9c442fd543c3d
mslau23 <112873828+mslau23@users.noreply.github.com>
2023-04-28 14:30:12 +0000

    encore

--- bdd.php
+++ bdd.php
@@ -12,9 +12,24 @@
  }
  //*************************************************
  //Requêtes SQL sur la base choisie
+ $valnom=$_POST['nom'];
+ $valdescription=$_POST['description'];
+ $valadresse=$_POST['adresse'];
+ $inserer = "INSERT INTO rando (nom, description, adresse) VALUES ( '$valnom','$valdescription','$valadresse');" ;
+ $resultat=$idcom->query($inserer);
+ $ordonner = "SELECT * FROM rando ORDER BY nom";
+ $resultat=$idcom->query($ordonner);
  //Lecture des résultats
+ echo "<ul>";
+ while($ligne = $resultat->fetch(PDO::FETCH_NUM)){
+    for($i=0; $i<count($ligne); $i++){
+        echo "<li>$ligne[$i]</li>";
+    }
+ }
+ echo "</ul>";
  //*************************************************
  //Fermeture de la connexion
+ $resultat->closeCursor();
  $idcom = NULL;
 ?>
 </html>
