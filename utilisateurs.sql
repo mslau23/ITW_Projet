ad4b20670e465f0e3e99f674a969de35f9fdad7f
mslau23 <112873828+mslau23@users.noreply.github.com>
2023-05-20 22:23:15 +0000

    ajout table utilisateurs

---
+++ utilisateurs.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `utilisateurs` (
+  `iduser` varchar(15) NOT NULL,
+  `mdpuser` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`iduser`, `mdpuser`) VALUES
+('root', 'root');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
