c244e31ce799513752ab80224f5cb6e069433a48
mslau23 <112873828+mslau23@users.noreply.github.com>
2023-05-21 10:05:03 +0000

    maj barre nav avec identifiant

--- Connexion.php
+++ Connexion.php
@@ -67,7 +67,7 @@
           if ($utilisateur && password_verify($motDePasse, $utilisateur['mot_de_passe'])) {
             // Connexion réussie
              // Stocker iduser dans la variable de session
-              $_SESSION['iduser'] = $iduser;
+              $_SESSION['iduser'] = $identifiant;
             // Initialiser la variable de session loggedIn
             session_start();
             $_SESSION['loggedIn'] = true;
