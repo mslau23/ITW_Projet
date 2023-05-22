cea42633b59546b41179e8a07c82ddab51e932e4
mslau23 <112873828+mslau23@users.noreply.github.com>
2023-05-22 07:59:06 +0000

    essai ajout redirection

--- action_score.php
+++ action_score.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="fr">
 <?php
+    session_start();
     if (isset($_POST['score'])) {
         $score = $_POST['score'];
-        session_start();
         $iduser = $_SESSION['iduser'];
     
     // Connexion à la base de données
@@ -15,9 +15,12 @@
         $requete = $dbh->prepare('UPDATE score SET score = :score WHERE id = :id_user AND rando = :rando');
         $requete->bindParam(':score', $score);
         $requete->bindParam(':id_user', $id_user);
-        $requete->bindParam(':rando', $randonnee);
+        $requete->bindParam(':rando', $rando);
         $requete->execute();
+    
+    // Redirection vers la page souhaitée après la mise à jour du score
+    header("Location: ".$rando.".php");
+    exit();
     }
     ?>
-    
 </html>
